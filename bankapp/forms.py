53ab573767eb8ac551c58941ca1417caf373e3de
Lord-baba99 <prcl.baba@gmail.com>
2023-07-26 14:57:24 +0100

    update

--- bankapp/forms.py
+++ bankapp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import *
 
+
 class BankOperationForm(forms.ModelForm):
 	class Meta:
 		model = BankOperation
+		fields = '__all__'
+
+
+class BankAccountForm(forms.ModelForm):
+	class Meta:
+		model = BankAccount
 		fields = '__all__'
--- cashdeskapp/forms.py
+++ cashdeskapp/forms.py
@@ -5,3 +5,9 @@ class CashDeskOperationForm(forms.ModelForm):
 	class Meta:
 		model = CashDeskOperation
 		fields = '__all__'
+
+
+class CashDeskForm(forms.ModelForm):
+	class Meta:
+		model = CashDesk
+		fields = '__all__'
